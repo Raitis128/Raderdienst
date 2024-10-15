ba66c6d0920f049f1821ee2913647b11bb77618c
Raitis <raitisiziks@gmail.com>
2024-10-16 00:27:28 +0300

    Add banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,10 +1,20 @@
-import { Heading, HStack, Image } from "@chakra-ui/react";
+import { Heading, HStack } from "@chakra-ui/react";
+import bannerImage from "../assets/images/tire3.jpg";
 
 const Banner = () => {
   return (
-    <HStack>
-      <Image />
-      <Heading as="h1" size="lg" bgColor={"blue.900"}>Text</Heading>
+    <HStack
+      backgroundImage={bannerImage}
+      bgRepeat={"no-repeat"}
+      bgPos={"center"}
+      bgSize={"cover"}
+      height="700px"
+      alignItems="flex-end"
+      justifyContent="center"
+    >
+      <Heading as="h1" size="xl" bgColor={"blue.800"} p={5} opacity={0.8}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </Heading>
     </HStack>
   );
 };
